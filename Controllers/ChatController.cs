5b2e71be88fe1b789a5d087bb186c360ea3cc531
Artur Buś <abuscitybus@gmail.com>
2018-01-09 11:49:57 +0100

    Dodaj pliki projektów.

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ChatController : ApiController
+    {
+        private arturDBEntities db = new arturDBEntities();
+
+    // GET: api/Chat
+      [EnableCors("*", "*", "GET")]
+      [Route("api/Chat/{id1}/{id2}")]
+
+    public IQueryable<Wiadomosci> GetWiadomoscis(int id1, int id2)
+      {
+        var wiadomosci = from w in db.Wiadomoscis
+          where w.Od == id1 && w.Do == id2 || w.Do == id1 && w.Od == id2
+                         orderby w.Data
+          select w;
+            return wiadomosci;
+        }
+      [EnableCors("*", "*", "PUT")]
+
+      public IHttpActionResult PutWiadomoscis(Wiadomosci wiadomosc)
+      {
+      if (!ModelState.IsValid)
+        {
+          return BadRequest(ModelState);
+        }
+      
+        db.Wiadomoscis.Add(wiadomosc);
+
+        try
+        {
+          db.SaveChanges();
+        
+        }
+        catch (Exception ex)
+        {
+        StringBuilder sb = new StringBuilder("Lini");
+          foreach (var error in db.GetValidationErrors())
+          {
+            sb.AppendLine(error.ToString());
+          }
+          return BadRequest(sb.ToString()+ ex.ToString());
+        }
+        return Ok();
+      }
+    // GET: api/Chat/5
+    [ResponseType(typeof(Wiadomosci))]
+        public IHttpActionResult GetWiadomosci(int id)
+        {
+            Wiadomosci wiadomosci = db.Wiadomoscis.Find(id);
+            if (wiadomosci == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(wiadomosci);
+        }
+
+        // PUT: api/Chat/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutWiadomosci(int id, Wiadomosci wiadomosci)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != wiadomosci.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(wiadomosci).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!WiadomosciExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Chat
+        [ResponseType(typeof(Wiadomosci))]
+        public IHttpActionResult PostWiadomosci(Wiadomosci wiadomosci)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Wiadomoscis.Add(wiadomosci);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (WiadomosciExists(wiadomosci.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = wiadomosci.Id }, wiadomosci);
+        }
+
+        // DELETE: api/Chat/5
+        [ResponseType(typeof(Wiadomosci))]
+        public IHttpActionResult DeleteWiadomosci(int id)
+        {
+            Wiadomosci wiadomosci = db.Wiadomoscis.Find(id);
+            if (wiadomosci == null)
+            {
+                return NotFound();
+            }
+
+            db.Wiadomoscis.Remove(wiadomosci);
+            db.SaveChanges();
+
+            return Ok(wiadomosci);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool WiadomosciExists(int id)
+        {
+            return db.Wiadomoscis.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Controllers/UzytkownicyController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+  public class UzytkownicyController : ApiController
+  {
+    private arturDBEntities db = new arturDBEntities();
+
+    [EnableCors("*", "*", "GET")]
+    [Route("api/Uzytkownicy/Login/{nazwa}/{haslo}")]
+    public IHttpActionResult GetLogin(string nazwa, string haslo)
+    {
+      var uzytkownicy = db.Uzytkownicies.Where(u => (u.Login == nazwa && u.Haslo == haslo)).FirstOrDefault();
+      if (uzytkownicy != null)
+        return Ok(uzytkownicy.Id);
+      else
+      {
+        return Ok(false);
+      }
+    }
+    // GET: api/Uzytkownicy
+    [EnableCors("*", "*", "GET")]
+    public IQueryable<object> GetUzytkownicies()
+    {
+      return db.Uzytkownicies.Select(x=> new {Id = x.Id, Login= x.Login});
+    }
+
+    // GET: api/Uzytkownicy/5
+    [ResponseType(typeof(Uzytkownicy))]
+    public IHttpActionResult GetUzytkownicy(int id)
+    {
+      Uzytkownicy uzytkownicy = db.Uzytkownicies.Find(id);
+      if (uzytkownicy == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(uzytkownicy);
+    }
+
+    // PUT: api/Uzytkownicy/5
+    [ResponseType(typeof(void))]
+    public IHttpActionResult PutUzytkownicy(int id, Uzytkownicy uzytkownicy)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      if (id != uzytkownicy.Id)
+      {
+        return BadRequest();
+      }
+
+      db.Entry(uzytkownicy).State = EntityState.Modified;
+
+      try
+      {
+        db.SaveChanges();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!UzytkownicyExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return StatusCode(HttpStatusCode.NoContent);
+    }
+
+    // POST: api/Uzytkownicy
+    [ResponseType(typeof(Uzytkownicy))]
+    public IHttpActionResult PostUzytkownicy(Uzytkownicy uzytkownicy)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      db.Uzytkownicies.Add(uzytkownicy);
+      db.SaveChanges();
+
+      return CreatedAtRoute("DefaultApi", new { id = uzytkownicy.Id }, uzytkownicy);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing)
+      {
+        db.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    private bool UzytkownicyExists(int id)
+    {
+      return db.Uzytkownicies.Count(e => e.Id == id) > 0;
+    }
+  }
+}
